3ca322ca5acbd050ccfb8806f9a8a71112006740
Aydın <aydinakyol11@gmail.com>
2021-05-04 14:34:46 +0300

    Features support (#13)
    * axios support
    
    * bundle analyzer support
    
    * env support
    
    * fetch support
    
    * reverse proxy support

--- plugins/fetch/src/components/examples/fetch/index.spec.tsx
+++ plugins/fetch/src/components/examples/fetch/index.spec.tsx
@@ -3,11 +3,11 @@ import "./mocks";
 import { mockResponse } from "./mocks";
 
 <%_ if (testing === 'testing-library') { _%>
-import { render, waitFor } from "@test";
+import { render, waitFor } from "test";
 <%_ } _%>
 
 <%_ if (testing === 'enzyme') { _%>
-import mount from "@test/mount";
+import mount from "test/mount";
 <%_ } _%>
     
 <%_ if (testing === 'testing-library') { _%>

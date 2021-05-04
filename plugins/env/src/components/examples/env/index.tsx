72da84df8eba37e54e225eeb23f8484311430265
Aydın Akyol <aydinakyol11@gmail.com>
2021-05-04 11:54:34 +0300

    env support

--- plugins/env/src/components/examples/env/index.tsx
+++ plugins/env/src/components/examples/env/index.tsx
@@ -3,30 +3,15 @@ import React from "react";
 export const EnvExample = () => {
     return (
         <div>
+            <div>REACT_APP_ENV_VARIABLE</div>
+            <div>{process.env.REACT_APP_ENV_VARIABLE}</div>
+            <div>REACT_APP_DEVELOPMENT_ENV_VARIABLE</div>
+            <div>{process.env.REACT_APP_DEVELOPMENT_ENV_VARIABLE}</div>
+            <div>REACT_APP_ENV_LOCAL_VARIABLE</div>
+            <div>{process.env.REACT_APP_ENV_LOCAL_VARIABLE}</div>
+            <div>REACT_APP_PRODUCTION_ENV_VARIABLE</div>
             <div>
-                NEXT_PUBLIC_ENV_VARIABLE
-                </div>
-            <div>
-                {process.env.NEXT_PUBLIC_ENV_VARIABLE}
-            </div>
-            <div>
-                NEXT_PUBLIC_ENV_LOCAL_VARIABLE
-                </div>
-            <div>
-                {process.env.NEXT_PUBLIC_ENV_LOCAL_VARIABLE}
-            </div>
-            <div>
-                NEXT_PUBLIC_DEVELOPMENT_ENV_VARIABLE
-                </div>
-            <div>
-                {process.env.NEXT_PUBLIC_DEVELOPMENT_ENV_VARIABLE}
-            </div>
-            <div>
-                NEXT_PUBLIC_PRODUCTION_ENV_VARIABLE
-                </div>
-            <div>
-                {process.env.NEXT_PUBLIC_PRODUCTION_ENV_VARIABLE ??
-                    "undefined"}
+                {process.env.REACT_APP_PRODUCTION_ENV_VARIABLE ?? "undefined"}
             </div>
         </div>
     );

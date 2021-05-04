743398b62bc8c744f6616dcad3315f775b3c2daf
Aydın <aydinakyol11@gmail.com>
2021-05-04 17:53:49 +0300

    mobx and mobx state tree support added (#18)

--- plugins/mobx/extend.js
+++ plugins/mobx/extend.js
@@ -1,11 +1,11 @@
 const base = {
     _app: {
-        import: ["import { RootStoreProvider } from '@mobx';"],
+        import: ["import { RootStoreProvider } from 'store';"],
         inner: [],
         wrapper: [["<RootStoreProvider>", "</RootStoreProvider>"]],
     },
     testSetup: {
-        import: ["import { RootStoreProvider } from '@mobx';"],
+        import: ["import { RootStoreProvider } from 'store';"],
         inner: [],
         wrapper: [["<RootStoreProvider>", "</RootStoreProvider>"]],
     },

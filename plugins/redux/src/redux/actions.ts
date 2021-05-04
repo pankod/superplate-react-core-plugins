bb1f66396ece59da193ba22a75283e8fd6b2ef4a
Aydın <aydinakyol11@gmail.com>
2021-05-04 13:41:43 +0300

    redux support (#15)

--- plugins/redux/src/redux/actions.ts
+++ plugins/redux/src/redux/actions.ts
@@ -1,1 +1,1 @@
-export { increase, decrease } from "@redux/slices/counter";
+export { increase, decrease } from "redux/slices/counter";
--- plugins/redux/src/redux/reducers.ts
+++ plugins/redux/src/redux/reducers.ts
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 
-import counter from "@redux/slices/counter";
+import counter from "redux/slices/counter";
 
 const rootReducer = combineReducers({ counter });
 

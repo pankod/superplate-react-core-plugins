06c1906faec3d9ae68312cac780edb67e3271095
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2021-05-04 15:24:53 +0300

    Updated ESLint message.
    Co-authored-by: Ömer Faruk APLAK <omer@pankod.com>

--- prompt.js
+++ prompt.js
@@ -112,7 +112,7 @@ module.exports = {
             hint: "use <arrow-keys> to navigate, <space> to select.",
             pageSize: 10,
             choices: [
-                { message: "ESLint", name: "eslint" },
+                { message: "ESLint (built-in support)", name: "eslint" },
                 { message: "Lint staged files", name: "lint-staged" },
             ],
             default: "eslint",

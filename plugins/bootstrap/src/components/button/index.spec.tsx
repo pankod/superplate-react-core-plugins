9047277e2cb179bee379e5ce9ad1020bd975b7e4
Aydın Akyol <aydinakyol11@gmail.com>
2021-05-07 12:22:49 +0300

    remove enzyme from tests

--- plugins/bootstrap/src/components/button/index.spec.tsx
+++ plugins/bootstrap/src/components/button/index.spec.tsx
@@ -1,8 +1,6 @@
 <%_ if (testing === 'testing-library') { _%>
-import { fireEvent, render } from "@test";
-<%_ } else if (testing === 'enzyme') {_%>
-import mount from "@test/mount";
-<%_ } _%>
+import { fireEvent, render } from "test";
+<%_ }_%>
 
 import { Button } from "./index";
 
@@ -27,24 +25,4 @@ describe("Button component testing with testing-library", () => {
         expect(mockFn).toHaveBeenCalledTimes(1);
     });
 });
-<%_ } else if (testing === 'enzyme') { _%>
-describe("Button component testing with enzyme", () => {
-
-    it("renders without crashing", () => {
-        const component = mount(
-            <Button onClick={() => undefined} />
-        );
-  
-        expect(component).toBeTruthy();
-    });
-  
-    it("button is clickable", () => {
-        const mockFn = jest.fn();
-        const btn = mount(<Button onClick={mockFn} />);
-    
-        btn.simulate("click");
-    
-        expect(mockFn).toHaveBeenCalledTimes(1);
-    });
-});
-<%_ } _%>
+<%_ }_%>
